f5c3766a641a0d69b3fb6f52e67680553e570fe4
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-04-03 15:38:58 -0400

    complete

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function removeOddSum() {
+//cannot use DOT notation in this case
+function removeOddSum(numArray, a, b) {
+  let output = numArray.filter( elem =>
+     (elem[a] + elem[b])%2 === 0
+  );
+  return output;
 }
 
 module.exports = removeOddSum;
